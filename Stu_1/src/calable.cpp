05aaa79b3afe02528e613e11fe9279676fc86687
zhihaoli <zhihaoli13@outlook.com>
2023-03-25 10:02:23 +0800

    类的变量调用

--- Stu_1/src/calable.cpp
+++ Stu_1/src/calable.cpp
@@ -6,7 +6,20 @@ Cala::Cala_na::Calable::Calable(){
 };
 
 void Cala::Cala_na::Calable::fun(){
-
+    
+    Cala::Calz::Compute cm;
+    cout<<cm.cat_num<<endl;
     cout<<"实现方法"<<endl;
+
+}
+
+Cala::Calz::Compute::Compute(){
+    this->neibor="私有成员变量";
+    cout<<"构构造函数"<<endl;
+}
+
+double Cala::Calz::Compute::sum(double a,double b){
+    // cout<<neibor<<endl;
+    return a+b;
 
 }
--- Stu_1/src/calable.h
+++ Stu_1/src/calable.h
@@ -1,6 +1,6 @@
-#include<iostream>
 #ifndef CALABLE_H
 #define CALABLE_H
+#include<iostream>
 using namespace std;
 
 namespace Cala{
@@ -8,11 +8,25 @@ namespace Cala_na{
     class Calable
     {
     private:
-        int num = 0;
+        int num;
+
     public:
         Calable();
         void fun();
     };
 } // namespace name
+
+namespace Calz{
+
+    class Compute{
+        private:
+            string neibor;
+        public:
+            int cat_num =90;
+            Compute();
+            double sum(double a,double b);
+    };
+    // int Compute::cat_num=90;
+}
 }
 #endif
